77fb0ef4b0858190af47adb7291aa6914dbd231f
Jeff Siver <jeff@siverfamily.com>
2016-06-19 22:59:16 -0500

    Adding test for non generic enumerable

--- ObjectPrinter.Tests/EnumerableTest.cs
+++ ObjectPrinter.Tests/EnumerableTest.cs
@@ -1,28 +1,35 @@
-﻿using FluentAssertions;
+﻿using System.Collections.Generic;
+using FluentAssertions;
 using Siver.Jeff.ObjectPrinter.Tests.Objects;
 using Xunit;
+using System.Collections;
 
 namespace Siver.Jeff.ObjectPrinter.Tests
 {
-    public class GenericEnumerableTest
+    public class EnumerableTest
     {
         [Fact]
         public void ShouldHandleEnumerableWithSimpleType()
         {
-            var withEnumerable = WithEnumerable.BuildWithData();
             var prettyPrinter = new PrettyPrinter();
-            var result = prettyPrinter.Print(withEnumerable);
+            var result = prettyPrinter.Print(WithEnumerable.BuildWithData());
             result.Should().Be("StringList: [ first, second, third,  ]");
         }
 
         [Fact]
         public void ShouldHandleEnumerableWithComplexType()
         {
-            var input = ComplexEnumerable.Build();
             var prettyPrinter = new PrettyPrinter();
-            var result = prettyPrinter.Print(input);
+            var result = prettyPrinter.Print(ComplexEnumerable.Build());
             result.Should().Be("Id: 12; Name: the name of this is name; Values: [ first, second ]; SimpleList: [ AnInteger: 1,012; AString: string; ADateTime: 2016-04-12T14:15:16.0000000Z; ADouble: 1,015.12; MyEnum: Value2 ]");
         }
 
+        [Fact]
+        public void ShouldHandleNonGenericEnumerable()
+        {
+            var prettyPrinter = new PrettyPrinter();
+            var result = prettyPrinter.Print((IEnumerable) new [] {"one", "two", "three"});
+            result.Should().Be("[ one, two, three ]");
+        }
     }
 }
